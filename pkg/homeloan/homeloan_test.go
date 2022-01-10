8a28ceb35e1ae61c41aa7fa0f8731a51fafcaa8b
Samarth Deyagond <deyagondsamarth@gmail.com>
2022-01-10 23:04:40 +0530

    Add more unit tests

--- pkg/homeloan/homeloan_test.go
+++ pkg/homeloan/homeloan_test.go
@@ -21,18 +21,32 @@ var _ = Describe("Homeloan", func() {
 	Describe("HandlePOST", func() {
 		BeforeEach(func() {
 			handler = http.HandlerFunc(homeloan.Strategies)
+			writer = httptest.NewRecorder()
+		})
+
+		It("Processes a POST request successfully", func() {
 			data := homeloan.Data{
 				AnnualInterestRate: 7.15,
 				LoanAmount:         5000000,
 				LoanTenure:         15,
 			}
-			dataBytes, _ := json.Marshal(data)
-			req = httptest.NewRequest(http.MethodPost, "/strategies", bytes.NewBuffer(dataBytes))
-			writer = httptest.NewRecorder()
-		})
-		It("Processes a POST request successfully", func() {
+			req = buildRequest(data)
 			handler.ServeHTTP(writer, req)
 			Expect(writer.Code).To(Equal(http.StatusOK))
 		})
+
+		It("Processes a POST request with invalid request body", func() {
+			dataBytes := []byte(`{"annual_interest":"7.15", "loan_amount":"5000000","loan_tenure":"15"}`)
+			req := httptest.NewRequest(http.MethodPost, "/strategies", bytes.NewBuffer(dataBytes))
+			handler.ServeHTTP(writer, req)
+			Expect(writer.Code).To(Equal(http.StatusBadRequest))
+		})
 	})
 })
+
+func buildRequest(data homeloan.Data) *http.Request {
+
+	dataBytes, _ := json.Marshal(data)
+	req := httptest.NewRequest(http.MethodPost, "/strategies", bytes.NewBuffer(dataBytes))
+	return req
+}
