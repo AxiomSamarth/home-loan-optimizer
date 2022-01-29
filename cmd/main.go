1bec15586d059c1adf8f9bf347f7510482b875c6
Samarth Deyagond <deyagondsamarth@gmail.com>
2022-01-29 11:59:30 +0530

    configured port

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	homeloan "github.com/AxiomSamarth/home-loan-optimizer/pkg/homeloan"
 )
 
 func handleRequests() {
+	port := ":" + os.Getenv("PORT")
 	http.HandleFunc("/", homeloan.Home)
 	http.HandleFunc("/strategies", homeloan.Strategies)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(port, nil))
 }
 
 func main() {
